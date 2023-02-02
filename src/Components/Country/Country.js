04a3b9b312aa6f68e70a033ea4d477412b278c60
antu <antusaha970@gmail.com>
2023-02-02 12:04:09 +0600

    Country Details area coded

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../Country/Country.css';
 
 
 
 const Country = (props) => {
-   const customStyle = {
-     margin: '20px',
-     borderBottom: '1px solid black',
-   }
+    const customStyle = {
+        margin: '20px',
+        borderBottom: '1px solid black',
+    }
     const name = props.country.name.common;
     const flag = props.country.flags.png;
 
     return (
         <>
-        <div className="container text-center" style={customStyle}>
-            <img src={flag} alt=""  />
-            <h2>{name}</h2>
-            <Link to={`country/${name}`}><button className='btn btn-success btn-sm mb-3'>View Details</button></Link>
-        </div>
+            <div className="container text-center" style={customStyle}>
+                <div className="row">
+                    <div className="col-md-12">
+                        <img src={flag} alt="" className='img-area' />
+                        <h2>{name}</h2>
+                        <Link to={`country/${name}`}><button className='btn btn-success btn-sm mb-3'>View Details</button></Link>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
--- src/Components/CountryDetails/CountryDetails.js
+++ src/Components/CountryDetails/CountryDetails.js
@@ -1,25 +1,58 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const CountryDetails = () => {
-    const {name} = useParams();
+    const { name } = useParams();
     const [country, setCountry] = useState({});
-    useEffect(()=>{
-        const url = `https://restcountries.com/v3.1/name/${name}`;
+    useEffect(() => {
+        const url = `https://restcountries.com/v3.1/name/${name}?fullText=true`;
         fetch(url)
-        .then(response => response.json())
-        .then(data => setCountry(data[0]));
-    },[])
-    const countryName = country.name.common;
-    const flag = country.flags.png;
+            .then(response => response.json())
+            .then(data => setCountry(data[0]));
+    }, [])
 
+
+    const countryName = country.name?.official;
+    const flag = country.flags?.png;
+    let lang;
+    if (country.languages !== undefined) {
+        lang = Object.values(country.languages);
+    }
+    let currency;
+    if (country.currencies !== undefined) {
+        currency = Object.keys(country.currencies);
+    }
 
     return (
         <div>
-            <div className="container text-center">
+            <div className="container text-center mb-3">
                 <img src={flag} alt="" />
-                <h3>{countryName}</h3>
+                <h1>{countryName}</h1>
+                {country.continents &&
+                    <p>Continents : {country.continents}</p>
+                }
+                {country.capital && <p>Capital : {country.capital[0]}</p>}
+                {
+                    country.population && <p>Population : {country.population}</p>
+                }
+
+                {
+                    country.area && <p>Area : {country.area}</p>
+                }
+                {
+                    country.timezones && <p>Timezone : {country.timezones[0]}</p>
+                }
+                {
+                    lang && <p>Languages : {lang.map(lg => <span>{lg}, </span>)}</p>
+                }
+                {
+                    country.currencies && <p>Currency : {country.currencies[currency].symbol}  {country.currencies[currency].name}</p>
+                }
+                {
+                    country.maps && <a href={country.maps?.googleMaps} target="_blank"><button className='btn btn-success'>View map on google</button></a>
+                }
+
             </div>
         </div>
     );
